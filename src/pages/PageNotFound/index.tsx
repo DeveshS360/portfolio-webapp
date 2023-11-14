2b7ae2453c53378899ac6ce4df94ac5dd9edd4bf
Devessh <devesh.sharma@wintwealth.com>
2023-11-14 17:39:53 +0530

    informative text + image added

--- src/pages/PageNotFound/index.tsx
+++ src/pages/PageNotFound/index.tsx
@@ -1,6 +1,7 @@
 import { PageHeader } from 'src/components/PageHeader'
 import styles from './index.module.scss'
 import { Footer } from 'src/components/Footer'
+import notFoundImage from 'assets/images/404-error.webp'
 
 export const PageNotFound = () => {
   return (
@@ -11,7 +12,20 @@ export const PageNotFound = () => {
         redirectionRoute="/"
         currPage="404"
       />
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <h1 className={styles.title}>Sorry!</h1>
+          <h2 className={styles.subtitle}>This Page is Not Found.</h2>
+          <p className={styles.description}>
+            The page you're looking for does not exist or seems to be down. We
+            request you to please check the URL once again or else try again
+            after sometime!
+          </p>
+        </div>
+        <div className={styles.astronaut}>
+          <img width={'100%'} src={notFoundImage} />
+        </div>
+      </div>
       <Footer />
     </section>
   )
