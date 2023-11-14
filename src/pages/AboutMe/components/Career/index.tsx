292f1de1f2e7502484f888e96db06918622b77f3
Devessh <devesh.sharma@wintwealth.com>
2023-11-14 13:31:00 +0530

    expertise UI with dummy content

--- src/pages/AboutMe/components/Career/index.tsx
+++ src/pages/AboutMe/components/Career/index.tsx
@@ -16,7 +16,7 @@ export const Career = () => {
 
   return (
     <section className={cx(layoutStyles.introduction, styles.career)}>
-      <div className={styles.content}>
+      <div className={styles.toggle_exptertise}>
         <div className={styles.btn_container}>
           <button
             className={cx(styles.expertise, {
@@ -36,6 +36,48 @@ export const Career = () => {
           >
             Education
           </button>
+        </div>
+      </div>
+      <div className={styles.chronology_container}>
+        <div className={styles.chronology}>
+          <div className={styles.timeline}>
+            <h6 className={styles.duration}>2010 - 2012</h6>
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>
+              Bachelor of Business Administration
+            </h1>
+            <h3 className={styles.subtitle}>
+              University of California, Berkeley
+            </h3>
+            <p className={styles.description}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been indus dard dummy text ever since
+              the 1500 when an uniknown prnter took galley of type and scrambled
+              make specimen book has not only five centuries the into
+              electronic.
+            </p>
+          </div>
+        </div>
+        <div className={styles.chronology}>
+          <div className={styles.timeline}>
+            <h6 className={styles.duration}>2012 - 2016</h6>
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>
+              Bachelor of Business Administration
+            </h1>
+            <h3 className={styles.subtitle}>
+              University of California, Berkeley
+            </h3>
+            <p className={styles.description}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been indus dard dummy text ever since
+              the 1500 when an uniknown prnter took galley of type and scrambled
+              make specimen book has not only five centuries the into
+              electronic.
+            </p>
+          </div>
         </div>
       </div>
     </section>
