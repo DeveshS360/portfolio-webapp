2f061699a17f0548b945d43e8d9039e6e0ee9bf2
Devesh Sharma <34626595+DeveshS360@users.noreply.github.com>
2023-11-18 00:15:11 +0530

    FAQ page (#17)
    * FAQ page
    
    * small text change

---
+++ src/pages/FAQ/index.tsx
@@ -0,0 +1,25 @@
+import { PageHeader } from 'src/components/PageHeader'
+import styles from './index.module.scss'
+import { AppRoutes } from 'src/constants/routes'
+import { FAQ_LIST } from 'src/constants/faq'
+import { FaqCard } from 'src/components/FaqCard'
+import { Footer } from 'src/components/Footer'
+
+export const FAQ = () => {
+  return (
+    <section className={styles.faq}>
+      <PageHeader
+        title="FAQs"
+        currPage="FAQs"
+        redirectionPage="Home"
+        redirectionRoute={AppRoutes.home}
+      />
+      <div className={styles.content}>
+        {FAQ_LIST.map((faq) => (
+          <FaqCard {...faq} />
+        ))}
+      </div>
+      <Footer />
+    </section>
+  )
+}
