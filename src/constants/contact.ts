c662e55fc4984b6ae76fb0f44b564f46887903b7
Devessh <devesh.sharma@wintwealth.com>
2023-11-18 14:02:20 +0530

    contact me ways added

---
+++ src/constants/contact.ts
@@ -0,0 +1,28 @@
+import { LINKEDIN_URL } from './common'
+import { PROJECT_URLS } from './projects'
+
+export const CONTACT_WAYS: Record<string, string>[] = [
+  {
+    key: 'Phone',
+    value: '+91 8619431049',
+  },
+
+  {
+    key: 'Email',
+    value: 'sharmadevesh360@gmail.com',
+  },
+  {
+    key: 'Webiste',
+    value: PROJECT_URLS.portfolioWebApp,
+  },
+  {
+    key: 'LinkedIn',
+    value: LINKEDIN_URL,
+  },
+
+  {
+    key: 'Address',
+    value:
+      'Sri Ganappa Residency, Old Madiwala, BTM 1st stage, Bengaluru, India',
+  },
+]
--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -1,14 +1,30 @@
 import { GoogleMapLocation } from 'src/components/GoogleMapLocation'
 import { PageHeader } from 'src/components/PageHeader'
 import styles from './index.module.scss'
+import { CONTACT_WAYS } from 'src/constants/contact'
+import { Footer } from 'src/components/Footer'
 
 export const ContactMe = () => {
   return (
     <div className={styles.contact_me}>
       <PageHeader title="Contact Me" currPage="Contact" />
       <div className={styles.content}>
-        <GoogleMapLocation />
+        <section>
+          <GoogleMapLocation />
+        </section>
+        <section className={styles.contact_ways}>
+          {CONTACT_WAYS.map((contact) => {
+            const { key, value } = contact
+            return (
+              <div key={key} className={styles.card}>
+                <div className={styles.title}>{key}:</div>
+                <p className={styles.subtitle}>{value}</p>
+              </div>
+            )
+          })}
+        </section>
       </div>
+      <Footer />
     </div>
   )
 }
